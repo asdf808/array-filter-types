8e522d81407c52adfd33a1aa0e5b00e9d34642f7
Roy.Liu <Roy.Liu@fanruan.com>
2023-07-18 00:16:24 +0800

    通过类型检查

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ interface User {
   age: number;
 }
 
+const OriginBoolean = Boolean;
+
 export function getAdults(users: (User | null)[]): User[] {
+
+  const Boolean = <T>(value: T): value is NonNullable<T> => OriginBoolean(value);
+
   return users.filter(Boolean).filter((user) => user.age >= 18);
 }
